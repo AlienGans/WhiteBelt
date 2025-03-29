6bb1a46c4de981a742ba450cc52ec0106ef902e0
AlienGans <gdeliver@inbox.ru>
2025-03-29 18:54:33 +0300

    again

--- Week#3/Proj3.4.2 First_names and Last_names - 1/sourse2.cpp
+++ Week#3/Proj3.4.2 First_names and Last_names - 1/sourse2.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-#include <map>
 #include <vector>
+#include <map>
+#include <algorithm>
 using namespace std;
 
 class Person
@@ -8,98 +9,79 @@ class Person
     public:
     void ChangeFirstName(int year, const string& first_name)
     {
-       if (!mp_fn.count(year))
-       {
-            mp_fn[year] = first_name;
-       }
+        name[year] = first_name;
+        n_change.push_back(year);
+        sort(begin(n_change), end(n_change));
     }
-    void ChangeLastName(int year, const string& last_name)
-    {
-        if (!mp_ln.count(year))
-       {
-            mp_ln[year] = last_name;
-       }
+    void ChangeLastName(int year, const string& Last_name)
+    { 
+        surname[year] = Last_name;
+        surn_change.push_back(year);
+        sort(begin(surn_change), end(surn_change));
     }
     string GetFullName(int year)
     {
-        if (!mp_fn.empty() && !mp_ln.empty())
+        //                                   ##### 1 этап
+        int max_name = 0;        
+        for (auto i : n_change)
         {
-            int temp = 0;
-            for (const auto& i : mp_fn)
-            {
-                temp = i.first; break;
-            }
+            if (i <= year) max_name = i;
+        }
+        
+        int max_surname = 0;
+        for (auto i : surn_change)
+        {
+            if (i <= year) max_surname = i;
+        }
+        
+        //                                   #### 2 этап
+        if (max_name == 0 && max_surname == 0)
+        {
+            return "Incognito";
+        }
 
-            int temp2 = 0;
-            for (const auto& i : mp_ln)
-            {
-                temp2 = i.first; break;
-            }
-            
-            if(year < temp && year < temp2)
-            {
-                return "Incognito";
-            }
-            else
-            {
-                mp_fn.size();
-                mp_ln.size();
-            }
-            
-        }  
+        if (max_name != 0 && max_surname != 0)
+        {
+            return name[max_name] + ' ' + surname[max_surname];
+        }
+
+        if (max_name == 0 && max_surname != 0)
+        {
+            return surname[max_surname] + " with unknown first name";
+        }
+
+        if (max_name != 0 && max_surname == 0)
+        {
+            return name[max_name] + " with unknown last name";
+        }
+        
     }
-
-
-
-    // else if (mp_fn.count(year) && !mp_ln.count(year))
-    //         {
-    //             if (vec_fln.empty())
-    //             {
-    //                 vec_fln.push_back(mp_fn[year]);
-    //             }
-    //             else
-    //             {
-    //                 vec_fln[0] = mp_fn[year];
-    //             }
-            
-    //             return mp_fn[year] + " with unknown last name";
-    //         }
-    //         else if (!mp_fn.count(year) && mp_ln.count(year))
-    //         {
-    //             return mp_ln[year] + " with unknown first name";
-    //         }
-    //         else
-    //         {
-                
-    //         }
-
-
     private:
-    vector<string> vec_fln; // [0] - first name; [1] - last name
-    //map<int, vector<string>> mp_fln;
-    map<int, string> mp_fn;
-    map<int, string> mp_ln;
+    map<int, string> name;
+    map<int, string> surname;
+    vector<int> n_change;
+    vector<int> surn_change;
 };
+
 
 int main()
 {
     Person person;
-
     person.ChangeFirstName(1965, "Polina");
     person.ChangeLastName(1967, "Sergeeva");
-    for (int year : {1900, 1965, 1910}) {
+    for (int year : {1900, 1965, 1990}) {
         cout << person.GetFullName(year) << endl;
     }
-    
-    // person.ChangeFirstName(1970, "Appolinaria");
-    // for (int year : {1969, 1970}) {
-    //     cout << person.GetFullName(year) << endl;
-    // }
-    
-    // person.ChangeLastName(1968, "Volkova");
-    // for (int year : {1969, 1970}) {
-    //     cout << person.GetFullName(year) << endl;
-    // }
+  
+    person.ChangeFirstName(1970, "Appolinaria");
+    for (int year : {1969, 1970}) {
+        cout << person.GetFullName(year) << endl;
+    }
+  
+    person.ChangeLastName(1968, "Volkova");
+    for (int year : {1969, 1970}) {
+        cout << person.GetFullName(year) << endl;
+    }
 
     return 0;
 }
